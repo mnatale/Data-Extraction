8f3e46ee1a735f1d806d54a2bcc994f1a666eb5b
Mark Natale <mnatalex54@gmail.com>
2016-03-11 09:59:17 -0500

    Cleaned up code for PEP8 rules

--- template-fbi-ucr.py
+++ template-fbi-ucr.py
@@ -1,4 +1,4 @@
-print ("#!/usr/bin/python \n\
+print("#!/usr/bin/python \n\
 \n\
 ################################################################################\n\
 # Requires Python 3.x or later\n\
@@ -23,7 +23,7 @@ import sys\n\
 # None\n\
 \n\
 # Global var init\n\
-Dlist = {}\n\
+dlist = {}\n\
 \n\
 # Get the file name from argv[1] and open file handle.\n\
 if len(sys.argv) > 1:\n\
@@ -34,23 +34,21 @@ else:\n\
 fhobj = open(finname, \"r\")\n\
 ")
 
-print ("\n\
+print("\n\
 ##############################################################################\n\
 # Insert header from data table here to auto-gen custom file.\n\
 ")
-HeaderStr = "Year,Population,Violent crime total,Murder and nonnegligent Manslaughter,Forcible rape,Robbery,Aggravated assault,Property crime total,Burglary,Larceny-theft,Motor vehicle theft,Violent Crime rate,Murder and nonnegligent manslaughter rate,Forcible rape rate,Robbery rate,Aggravated assault rate,Property crime rate,Burglary rate,Larceny-theft rate,Motor vehicle theft rate"
+header_str = "Year,Population,Violent crime total,Murder and nonnegligent Manslaughter,Forcible rape,Robbery,Aggravated assault,Property crime total,Burglary,Larceny-theft,Motor vehicle theft,Violent Crime rate,Murder and nonnegligent manslaughter rate,Forcible rape rate,Robbery rate,Aggravated assault rate,Property crime rate,Burglary rate,Larceny-theft rate,Motor vehicle theft rate"
 
-PrependHeader = "print (\"State\",\"\\t\",\""
+prepend_header = "print (\"State\",\"\\t\",\""
 
+append_header = "\")"
+new_header = ""
+for i in header_str.replace(",", "\",\"\\t\",\""):
+    new_header = new_header + i
+print(prepend_header + new_header + append_header)
 
-AppendHeader = "\")"
-newheader = ""
-for i in HeaderStr.replace(",","\",\"\\t\",\""):
-	newheader = newheader + i	
-print(PrependHeader + newheader + AppendHeader)
-
-
-print ("\n\
+print("\n\
 \n\
 \n\
 ##############################################################################\n\
@@ -75,22 +73,22 @@ while True:\n\
 ##############################################################################\n\
 ")
 
-print ("\n\
+print("\n\
 	# Isolate  line containing State. Join two word state names and WDC.\n\
 	if line_list[0] ==\"Estimated\":\n\
 \n\
-		if line_list[3] == \"New\":\n\
-			Dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
+		if line_list[3] == \"NEW\":\n\
+			dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
 		elif line_list[3] == \"North\":\n\
-			Dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
+			dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
 		elif line_list[3] == \"South\":\n\
-			Dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
+			dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
 		elif line_list[3] == \"West\":\n\
-			Dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
+			dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
 		elif line_list[3] == \"District\":\n\
-			Dlist['STATE'] = line_list[3] + \" \" +  line_list[4] + \" \" +  line_list[5]\n\
+			dlist['STATE'] = line_list[3] + \" \" +  line_list[4] + \" \" +  line_list[5]\n\
 		else:\n\
-			Dlist['STATE'] = line_list[3]\n\
+			dlist['STATE'] = line_list[3]\n\
 		continue\n\
 \n\
 	# Exclude repeating data headers\n\
@@ -102,38 +100,38 @@ print ("\n\
 # Assemble the data reference to dictonary name string
 #
 ################################################################################
-AppendList = "'] = line_list[%d]"
-newList = "\tDlist['"
-for i in HeaderStr.replace(",","']  = line_list[%d]\n\tDlist['"):
-	newList = newList + i
-newList = newList + AppendList
+append_list = "'] = line_list[%d]"
+new_list = "\tdlist['"
+for i in header_str.replace(",", "'] = line_list[%d]\n\tdlist['"):
+    new_list = new_list + i
+new_list = new_list + append_list
 
-# Now that the 'Dlist' string is fully assembled, need to place the line_list 
+# Now that the 'dlist' string is fully assembled, need to place the line_list
 # index values in the string.
 num = 0
-for x in newList:
-	newList = newList.replace("%d", str(num), 1)
-	num += 1
+for x in new_list:
+    new_list = new_list.replace("%d", str(num), 1)
+    num += 1
 
-newList = newList.replace("Dlist['Year']  = line_list", 
-																"Dlist['Year'] = \"1/31/\" + line_list")
+new_list = new_list.replace("dlist['Year']  = line_list",
+                            "dlist['Year'] = \"1/31/\" + line_list")
 
-print (newList)
+print(new_list)
 
-print ("\n\
+print("\n\
 ##############################################################################\n\
 # Gen the data vectors for output\n\
 ")
 
-PrependOutput = "\tprint (Dlist['STATE'],\"\\t\","
-AppendDlist = "'])"
-printList = "Dlist['"
-for i in HeaderStr.replace(",","'], \"\\t\", Dlist['"):
-	printList = printList + i
-printList = printList + AppendDlist
-print(PrependOutput + printList)
+prepend_list = "\tprint (dlist['STATE'],\"\\t\","
+append_dlist = "'])"
+print_list = "dlist['"
+for i in header_str.replace(",", "'], \"\\t\", dlist['"):
+    print_list = print_list + i
+print_list = print_list + append_dlist
+print(prepend_list + print_list)
 
-print ("\n\
+print("\n\
 fhobj.close()\n\
 ")
 
