cef27333b451269300f6eba36c1fcde87a087734
Mark Natale <mnatalex54@gmail.com>
2016-05-03 22:38:22 -0400

    Added argparse, defined functions, changed comments for template generation, sprinkled better comments here and there.

--- template-fbi-ucr.py
+++ template-fbi-ucr.py
@@ -31,127 +31,109 @@ Usage: Python template-fbi-ucr.py > fbi-ucr.py
 """
 
 # Template begins here
-print("#!/usr/bin/python\n\
-# Auto-generated Pyhthon codei\n\
-# Requires Python 3.x or later\n\
-# The following code is a brut force method to xtract strings from the output\n\
-# of a text file. Once extracted the strings are scrubbed and # formated for\n\
-# Excel or Tableau processing.\n\
-#\n\
-# Author: Mark Natale - Boston MA\n\
-# email: mnatalex54@gmail.com\n\
-#\n\
-# Copyright 2016\n\
-#\n\
-# Orignal work\n\
-# Revision: 0.2\n\
-\n\
-# Py Library Imports\n\
-import sys\n\
-\n\
-# Local imports\n\
-# None\n\
-\n\
-# Global var init\n\
-dlist = {}\n\
-\n\
-# Get the file name from argv[1] and open file handle.\n\
-if len(sys.argv) > 1:\n\
-    file_name = sys.argv[1]\n\
-else:\n\
-    sys.exit(\"File name required. Exit\")\n\
-\n\
-fhobj = open(file_name, \"r\")\n\
-")
+print("""#!/usr/bin/python
+# Auto-generated Python code
+# Requires Python 3.x or later
+# The following code is a brut force method to xtract strings from the output
+# of a text file. Once extracted the strings are scrubbed and # formated for
+# Excel or Tableau processing.
+#
+# Author: Mark Natale - Boston MA
+# email: mnatalex54@gmail.com
+#
+# Copyright 2016
+#
+# Py Library Imports
+import sys
+import argparse
 
-print("\n\
-# Header from row 1 of data table.\n\
-")
+# Global var init
+dlist = {}
+
+def openfile():
+    parser = argparse.ArgumentParser(description='Process comma separated text file.')
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    return open(args.filename, "r")
+
+def main(fhobj):
+# Header from row 1 of data table.
+""")
 
 # Insert comma separated header here
 header_str = "Year,Population,Violent crime total,Murder and nonnegligent Manslaughter,Forcible rape,Robbery,Aggravated assault,Property crime total,Burglary,Larceny-theft,Motor vehicle theft,Violent Crime rate,Murder and nonnegligent manslaughter rate,Forcible rape rate,Robbery rate,Aggravated assault rate,Property crime rate,Burglary rate,Larceny-theft rate,Motor vehicle theft rate"
 
-prepend_header = "print(\"State\", \",\", \""
+prepend_header = "    print(\"State\", \",\", \""
 append_header = "\", sep='')"
 new_header = ""
 
 for i in header_str.replace(",", "\", \",\", \""):
     new_header = new_header + i
 
-# Gen fully formed header
+# Gen fully formed header for 1st row
 print(prepend_header + new_header + append_header)
 
-print("\n\
-\n\
-# Main line processing loop.\n\
-while True:\n\
-    line = fhobj.readline()\n\
-    if len(line) > 400:\n\
-        print(len(line))\n\
-        sys.exit(\"Readline error: Line length is longer than expected.\")\n\
-\n\
-    if len(line) == 0:	# Check for zero length line as EOF and exit loop\n\
-        break\n\
-\n\
-    line_list = line.rstrip().replace(\",\", \" \").split()	# remove ',' and ws\n\
-\n\
-    if line_list == '\\n':    # bypass empty lines\n\
-        continue\n\
-    if line_list == []:\n\
-        continue\n\
-\n\
-")
+print("""
+# Main line processing loop.
+    for line in fhobj:
+        if len(line) > 400:
+            print(len(line))
+            sys.exit("Readline error: Line length is longer than expected.")
+				# split the comma separate line into a vector
+        line_content = line.rstrip().replace(",", " ").split()    # remove ',' and ws
 
-print("\n\
-	# Isolate  line containing State. Join two word state names and WDC.\n\
-    if line_list[0] == \"Estimated\":\n\
-\n\
-        if line_list[3] == \"NEW\":\n\
-            dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
-        elif line_list[3] == \"North\":\n\
-            dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
-        elif line_list[3] == \"South\":\n\
-            dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
-        elif line_list[3] == \"West\":\n\
-            dlist['STATE'] = line_list[3] + \" \" +  line_list[4]\n\
-        elif line_list[3] == \"District\":\n\
-            dlist['STATE'] = line_list[3] + \" \" +  line_list[4] + \" \" +  line_list[5]\n\
-        else:\n\
-            dlist['STATE'] = line_list[3]\n\
-        continue\n\
-\n\
-    # Exclude repeating data headers\n\
-    if line_list[0] == \"Year\":\n\
-        continue\n\
-")
+        # Filter out unnwanted lines
+        if line_content == '':    # filter blank lines
+            continue
+        if line_content == []:    #Filter out line full of nulls(,,,,,)
+            continue
+        if line_content[0] == "Year":
+            continue
 
-# Assemble the data reference to dictonary name string
-new_list = "    dlist['"
-append_list = "'] = line_list[%d]"
+        # Isolate  lines containing State. Join two word state names and WDC.
+        if line_content[0] == "Estimated":
+            if line_content[3] == "NEW":
+                dlist['STATE'] = line_content[3] + " " +  line_content[4]
+            elif line_content[3] == "North":
+                dlist['STATE'] = line_content[3] + " " +  line_content[4]
+            elif line_content[3] == "South":
+                dlist['STATE'] = line_content[3] + " " +  line_content[4]
+            elif line_content[3] == "West":
+                dlist['STATE'] = line_content[3] + " " +  line_content[4]
+            elif line_content[3] == "District":
+                dlist['STATE'] = line_content[3] + " " +  line_content[4] + " " +  line_content[5]
+            else:
+                dlist['STATE'] = line_content[3]
+            continue
+						""")
 
-for i in header_str.replace(",", "'] = line_list[%d]\n    dlist['"):
+# Assemble the data reference to dictonary name string.
+# Keep 8 spaces prepended to dlist
+new_list = "        dlist['"
+append_list = "'] = line_content[%d]"
+
+for i in header_str.replace(",", "'] = line_content[%d]\n        dlist['"):
     new_list = new_list + i
 
 new_list = new_list + append_list
 
-# Now that the 'dlist' string is fully assembled, need to place the line_list
+# Now that the 'dlist' string is fully assembled, need to place the line_content
 # index values in the string.
 num = 0
 for x in new_list:
     new_list = new_list.replace("%d", str(num), 1)
     num += 1
 
-new_list = new_list.replace("dlist['Year'] = line_list",
-                            "dlist['Year'] = \"12/31/\" + line_list")
+new_list = new_list.replace("dlist['Year'] = line_content",
+                            "dlist['Year'] = \"12/31/\" + line_content")
 
 print(new_list)
 
-print("\n\
-##############################################################################\n\
-# Gen the data vectors for output\n\
-")
+print("""
+# Gen the data for output
+""")
 
-prepend_list = "    print(dlist['STATE'], \",\", "
+prepend_list = "        print(dlist['STATE'], \",\", "
 append_dlist = "'], sep='')"
 print_list = "dlist['"
 
@@ -162,7 +144,8 @@ print_list = print_list + append_dlist
 
 print(prepend_list + print_list)
 
-print("\n\
-fhobj.close()\n\
-")
+print("""
+if __name__ == "__main__":
+    main(openfile())
+""")
 
